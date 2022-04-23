b2326d523a089e5da7d97e920152cf47850f75ab
Max <84664361+Maxtronaut@users.noreply.github.com>
2022-04-23 21:04:21 +0200

    Premier push

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+import os
+
+bot = commands.Bot(command_prefix = "!")
+
+intents = discord.Intents.default()
+intents.members = True
+
+initial_extensions = []
+
+for filename in os.listdir('./cogs'):
+   if filename.endswith('.py'):
+      initial_extensions.append("cogs." + filename[:-3])
+
+print(initial_extensions)
+
+if __name__ == '__main__':
+   for extension in initial_extensions:
+      bot.load_extension(extension)
+
+@bot.event
+async def on_ready():
+   await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name="Gros caca puant"))
+   print(bot.user.name + " est en ligne")
+
+# @bot.command()
+# async def ping(ctx):
+#    await ctx.send("Pong")
+
+bot.run("OTY2NzM5NDE0MjU0NDg1NTk0.YmGIPQ.caSPrz3mQYwFyRb3xqtjJcXc9OQ")  
